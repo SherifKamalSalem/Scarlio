25a928526384e7965865ce25913778f4d88d9787
Sherif Kamal <sherif.kamal.salem@gmail.com>
2018-11-17 03:24:34 +0200

    add terms and conditions VC

---
+++ Scarlio/TermsVC.swift
@@ -0,0 +1,18 @@
+//
+//  TermsVC.swift
+//  Scarlio
+//
+//  Created by Sherif Kamal on 11/17/18.
+//  Copyright © 2018 Sherif Kamal. All rights reserved.
+//
+
+import UIKit
+
+class TermsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationItem.largeTitleDisplayMode = .never
+    }
+}
